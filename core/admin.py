578b603631d29b5749931f8770c8949b1b7bba3c
Oliver Ni <oliver.ni@gmail.com>
2021-08-12 03:29:52 -0700

    Add org permissions

--- core/admin.py
+++ core/admin.py
@@ -30,7 +30,64 @@ class UserAdmin(BaseUserAdmin, DynamicArrayMixin):
     list_filter = ("is_staff", "is_superuser", "grad_year")
     search_fields = ("email", "first_name", "last_name")
     ordering = None
-    inlines = (MembershipAdmin,)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    class InlineLinkAdmin(admin.TabularInline, DynamicArrayMixin):
+        model = OrganizationLink
+        extra = 0
+
+    class AdvisorForm(forms.ModelForm):
+        class Meta:
+            fields = (
+                "advisors",
+                "admins",
+                "name",
+                "description",
+                "category",
+                "day",
+                "time",
+                "link",
+                "ical_links",
+            )
+
+    class AdminForm(forms.ModelForm):
+        class Meta:
+            fields = (
+                "admins",
+                "name",
+                "description",
+                "category",
+                "day",
+                "time",
+                "link",
+                "ical_links",
+            )
+
+    list_display = ("name", "type", "day", "time", "link")
+    list_filter = ("type", "day", "category")
+    autocomplete_fields = ("advisors", "admins")
+    inlines = (InlineLinkAdmin,)
+
+    def has_module_permission(self, request):
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        if obj is None:
+            return True
+        return obj.is_admin(request.user) or obj.is_advisor(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return self.has_view_permission(request, obj)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(Q(admins=request.user) | Q(advisors=request.user))
+
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser:
+            kwargs["form"] = self.AdvisorForm if obj.is_advisor(request.user) else self.AdminForm
+        return super().get_form(request, obj=obj, **kwargs)
 
 
 @admin.register(Event)
@@ -50,23 +107,6 @@ class EventAdmin(admin.ModelAdmin, DynamicArrayMixin):
         qr_svg = qrcode.make(f"lhs://{obj.code}", image_factory=SvgPathFillImage, box_size=50, border=0)
         uri_svg = DataURI.make("image/svg+xml", charset="UTF-8", base64=True, data=qr_svg.to_string())
         return mark_safe(f'<img src="{uri_svg}" alt="lhs://{obj.code}">')
-
-
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin, DynamicArrayMixin):
-    class InlineLinkAdmin(admin.TabularInline, DynamicArrayMixin):
-        model = OrganizationLink
-        extra = 0
-
-    list_display = ("name", "type", "day", "time", "link")
-    list_filter = ("type", "day", "category")
-    inlines = (InlineLinkAdmin,)
-
-
-# @admin.register(Poll)
-# class PollAdmin(admin.ModelAdmin, DynamicArrayMixin):
-#     list_display = ("description", "post")
-#     list_filter = ("post__organization",)
 
 
 @admin.register(Period)
