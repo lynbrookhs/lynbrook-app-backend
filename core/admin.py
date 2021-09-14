716a8599263feb4589016b09b6bf75dae3883c0e
Oliver Ni <oliver.ni@gmail.com>
2021-09-14 01:30:44 -0700

    Add created at

--- core/admin.py
+++ core/admin.py
@@ -363,6 +363,7 @@ class SubmissionAdmin(admin.ModelAdmin, DynamicArrayMixin):
 
     list_filter = (EventListFilter,)
     search_fields = ("event__name", "user__first_name", "user__last_name")
+    readonly_fields = ("created_at",)
     list_display = ("user", "event", "file", "created_at")
 
 
